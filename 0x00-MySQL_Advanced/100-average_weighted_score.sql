ac508a90f52816f6a08d977be39f950f2fc97691
luckys-lnz <lalucky22@gmail.com>
2024-10-18 00:08:30 +0000

    12. Average weighted score

---
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -0,0 +1,23 @@
+-- Write a SQL script that creates a stored procedure
+-- ComputeAverageWeightedScoreForUser that computes and
+-- store the average weighted score for a student.
+
+-- Deletes any existing procedure with same name
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
+
+-- Sets custom delimiter
+DELIMITER |
+
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(user_id INT)
+BEGIN
+    DECLARE w_avg_score FLOAT;
+    SET w_avg_score = (SELECT SUM(score * weight) / SUM(weight) 
+                        FROM users AS U 
+                        JOIN corrections as C ON U.id=C.user_id 
+                        JOIN projects AS P ON C.project_id=P.id 
+                        WHERE U.id=user_id);
+    UPDATE users SET average_score = w_avg_score WHERE id=user_id;
+END |
+
+-- Restores default delimiter
+DELIMITER ;
